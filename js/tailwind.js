0fd605bcc18788998daa9cd24c78dc5e33096137
EmanuelRodriguezBedeman <emanuel.rodriguez.bedeman@gmail.com>
2023-03-20 17:43:28 -0300

    Added Flowbite

--- js/tailwind.js
+++ js/tailwind.js
@@ -1,5 +1,8 @@
 module.exports = {
-    content: ['./src/**/*.{html,js}'],
+    content: [
+        './src/**/*.{html,js}',
+        "./node_modules/flowbite/**/*.js"
+    ],
     theme: {
         colors: {
             'blue': '#1fb6ff',
@@ -34,42 +37,8 @@ module.exports = {
             }
         }
     },
-}
-
-tailwind.config = {
-    content: ['./src/**/*.{html,js}'],
-    theme: {
-        colors: {
-            'blue': '#1fb6ff',
-            'purple': '#7e5bef',
-            'pink': '#ff49db',
-            'orange': '#ff7849',
-            'green': '#13ce66',
-            'yellow': '#ffc82c',
-            'gray-dark': '#273444',
-            'gray': '#8492a6',
-            'gray-light': '#d3dce6',
-            'darkwood': '#1A120B',
-            'wood': '#3C2A21',
-            'lightwood': '#D5CEA3',
-            'cream': '#E5E5CB',
-            'navy': '#324E7B',
-            'lightblue': '#86A6DF',
-            'cblue': '#5068A9',
-        },
-        fontFamily: {
-            sans: ['Graphik', 'sans-serif'],
-            serif: ['Merriweather', 'serif'],
-            nova: ["Proxima Nova", 'sans-serif'],
-        },
-        extend: {
-            spacing: {
-                '8xl': '96rem',
-                '9xl': '128rem',
-            },
-            borderRadius: {
-                '4xl': '2rem',
-            }
-        }
-    },
+    plugins: [
+        require('flowbite/plugin'),
+        require('@tailwindcss/line-clamp'),
+    ],
 }
