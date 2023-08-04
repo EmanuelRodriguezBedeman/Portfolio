9c76805ad5946ec0eec1f13c5ea7212e436d61e9
EmanuelRodriguezBedeman <emanuel.rodriguez.bedeman@gmail.com>
2023-08-04 12:09:43 -0300

    removed un-used things in tailwind.js

--- js/tailwind.js
+++ js/tailwind.js
@@ -16,42 +16,6 @@ tailwind.config = {
                     "900": "#1e3a8a"
                 }
             }
-        },
-        fontFamily: {
-            'body': [
-                'Merriweather',
-                'ui-sans-serif',
-                'system-ui',
-                '-apple-system',
-                'system-ui',
-                'Segoe UI',
-                'Roboto',
-                'Helvetica Neue',
-                'Arial',
-                'Noto Sans',
-                'sans-serif',
-                'Apple Color Emoji',
-                'Segoe UI Emoji',
-                'Segoe UI Symbol',
-                'Noto Color Emoji'
-            ],
-            'sans': [
-                'Merriweather',
-                'ui-sans-serif',
-                'system-ui',
-                '-apple-system',
-                'system-ui',
-                'Segoe UI',
-                'Roboto',
-                'Helvetica Neue',
-                'Arial',
-                'Noto Sans',
-                'sans-serif',
-                'Apple Color Emoji',
-                'Segoe UI Emoji',
-                'Segoe UI Symbol',
-                'Noto Color Emoji'
-            ]
         }
     }
 }
