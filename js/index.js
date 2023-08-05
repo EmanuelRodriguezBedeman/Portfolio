65a27d39989db574fe33cec618308019b5e9ad59
EmanuelRodriguezBedeman <emanuel.rodriguez.bedeman@gmail.com>
2023-08-05 15:49:39 -0300

    added spinner to button on click

---
+++ js/index.js
@@ -0,0 +1,8 @@
+// const buttonActive = document.getElementById("buttonActive")
+// const buttonLabel = document.getElementById("buttonLabel")
+// const messageButton = document.getElementById("messageButton")
+
+// messageButton.addEventListener("click", function () {
+//     buttonLabel.classList.toggle("hidden");
+//     buttonActive.classList.toggle("hidden")
+// });
