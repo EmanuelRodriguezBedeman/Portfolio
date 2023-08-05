d6d00b0b515f2359769cd596ac806d1309125b71
EmanuelRodriguezBedeman <emanuel.rodriguez.bedeman@gmail.com>
2023-08-05 16:11:41 -0300

    buttons changes on submit

--- js/index.js
+++ js/index.js
@@ -1,8 +1,6 @@
-// const buttonActive = document.getElementById("buttonActive")
-// const buttonLabel = document.getElementById("buttonLabel")
-// const messageButton = document.getElementById("messageButton")
-
-// messageButton.addEventListener("click", function () {
-//     buttonLabel.classList.toggle("hidden");
-//     buttonActive.classList.toggle("hidden")
-// });
+const buttonActive = document.getElementById("buttonActive")
+const buttonLabel = document.getElementById("buttonLabel")
+function changeLabel() {
+    buttonLabel.classList.toggle("hidden");
+    buttonActive.classList.toggle("hidden")
+}
