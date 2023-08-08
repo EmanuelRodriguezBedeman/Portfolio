dc52b68200968e37f68522953921a5ab4d60e8a7
EmanuelRodriguezBedeman <emanuel.rodriguez.bedeman@gmail.com>
2023-08-07 22:23:59 -0300

    fixed logo bug, changed tweets description

--- js/index.js
+++ js/index.js
@@ -26,9 +26,10 @@ document.addEventListener('scroll', () => {
     if (window.scrollY >= screenHeight * 0.80) {
         navBar.classList.remove("bg-transparent", "md:text-white");
         navBar.classList.add("bg-white", "md:text-gray-900");
-        erbLogo.src = "src/imgs/erb-logo.webp"
+        erbLogo.src = "src/imgs/erb-logo.webp";
     } else {
         navBar.classList.remove("bg-white", "md:text-gray-900");
         navBar.classList.add("bg-transparent", "md:text-white");
+        erbLogo.src = "src/imgs/erb-logo white.webp";
     }
 });
